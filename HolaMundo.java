60b4690799a65ac63293d4b77c60bfba00f0d3af
José Sandoval <Sandoval2993@gmail.com>
2020-08-25 11:31:52 -0500

    Saludo corregido

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@ public class HolaMundo{
 	public static void main(String[] args){
 		String naranja = "\033[33m";
 		String azul = "\033[34m";
-		System.out.println("Hola mundo!");
-		System.out.println("Aquí estamos haciendo pruebas con Git.!");
+		System.out.println(naranja + "Hola mundo!");
+		System.out.println(azul + "Aquí estamos haciendo pruebas con Git.!");
 }
 }
